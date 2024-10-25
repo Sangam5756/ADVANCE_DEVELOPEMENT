f4ce86befe2a7943c5cc5cae300a94108984a2df
sangam5756 <sangammunde3@gmail.com>
2024-10-25 21:50:08 +0530

    recoilExplorePerformance

--- Week7/frontend/src/App.jsx
+++ Week7/frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useContext, useState } from "react";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 import { countAtom } from "./store/atoms/count";
 
 const App = () => {
@@ -12,9 +17,8 @@ const App = () => {
   );
 };
 
-
 const Count = () => {
-  console.log("count component");
+  console.log("count component re-render");
   return (
     <div className="flex flex-col gap-5">
       <RenderCount />
@@ -23,11 +27,9 @@ const Count = () => {
   );
 };
 
-
-
-
 const RenderCount = () => {
   const count = useRecoilValue(countAtom);
+  console.log("Count re-render");
   return (
     <div className="flex  items-center justify-center">
       <p>{count}</p>
@@ -35,15 +37,35 @@ const RenderCount = () => {
   );
 };
 
+{
+  /**
+      when i using default usestate and importing count and setcount from useRecoilState
+      and updating the count then it rerendering the button component
+      but we dont need to rerender button component .
+      
+      How to Stop Rerendering it. so we can remove the count from component because when we are setting count
+      it again rerendering because of the updated  count
 
+      so now we only take setcount from useSetRecoilState
+      but there is one problem.
+      How To update count so we use function (prev) =>prev+1
+      so it will take prev value then update it with new value.
+      no need of count
+      PROBLEM SOLVE
+      NOW THE BUTTON IS NOT RERENDERING WHEN THE COUNT CHANGE
+
+  
+   */
+}
 
 const Button = () => {
-  const [count, setCount] = useRecoilState(countAtom);
+  console.log("button component re-render....");
+  const setCount = useSetRecoilState(countAtom);
   return (
     <div className="flex gap-3">
       <button
         onClick={() => {
-          setCount(count + 1);
+          setCount((count) => count + 1);
         }}
       >
         Increase
@@ -51,7 +73,7 @@ const Button = () => {
 
       <button
         onClick={() => {
-          setCount(count - 1);
+          setCount((count) => count - 1);
         }}
       >
         Decrese

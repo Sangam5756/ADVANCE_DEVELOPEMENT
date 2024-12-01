054dbd6e8266db5840b7ed51c099a0ccc635dad5
sangam5756 <sangammunde3@gmail.com>
2024-12-01 21:28:37 +0530

    websocket client-server msg transfer

--- Week 19/websocket/socket/dist/index.js
+++ Week 19/websocket/socket/dist/index.js
@@ -1,10 +1,34 @@
 "use strict";
-// import WebSocket, { WebSocketServer } from "ws";
-// import http from "http";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// USING EXPRESS
+const express_1 = __importDefault(require("express"));
+const ws_1 = require("ws");
+const app = (0, express_1.default)();
+const httpserver = app.listen(8080, () => {
+    console.log(((new Date()) + "server is listening on 8080"));
+});
+const wss = new ws_1.WebSocketServer({ server: httpserver });
+let usercount = 0;
+wss.on("connection", (ws) => {
+    ws.on("error", console.error);
+    ws.on("message", (data, isbinary) => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === ws_1.WebSocket.OPEN) {
+                client.send(data, { binary: isbinary });
+            }
+        });
+    });
+    console.log("user count", ++usercount);
+    ws.off("close", () => {
+        console.log("user count", --usercount);
+    });
+    // ws.send("Hello From The Server")
+});
+// import WebSocket, { WebSocketServer } from "ws";
+// import http from "http";
 // const server = http.createServer((req, res) => {
 //     console.log((new Date()) + "Received Request for" + req.url)
 //     res.end("hi there");
@@ -26,15 +50,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // server.listen(8080, function () {
 //     console.log((new Date()) + ' Server is listening on port 8080');
 // });
-// USING EXPRESS
-const express_1 = __importDefault(require("express"));
-const ws_1 = require("ws");
-const app = (0, express_1.default)();
-const httpserver = app.listen(8080);
-const wss = new ws_1.WebSocketServer({ server: httpserver });
-wss.on("connection", (ws) => {
-    ws.on("error", console.error);
-    ws.on("message", (data, isbinary) => {
-        console.log("received :%s", data);
-    });
-});
--- Week 19/websocket/socket/src/index.ts
+++ Week 19/websocket/socket/src/index.ts
@@ -1,3 +1,38 @@
+// USING EXPRESS
+import express from "express"
+import { WebSocket, WebSocketServer } from "ws"
+
+
+const app = express();
+const httpserver = app.listen(8080, () => {
+    console.log(((new Date()) + "server is listening on 8080"))
+})
+
+const wss = new WebSocketServer({ server: httpserver })
+let usercount = 0;
+
+wss.on("connection", (ws) => {
+
+    ws.on("error", console.error)
+
+    ws.on("message", (data, isbinary) => {
+
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isbinary })
+            }
+        });
+
+    });
+    console.log("user count", ++usercount);
+ws.close("",()=>{
+    console.log("user count", --usercount);
+
+})
+    // ws.send("Hello From The Server")
+});
+
+
 // import WebSocket, { WebSocketServer } from "ws";
 // import http from "http";
 
@@ -29,21 +64,3 @@
 //     console.log((new Date()) + ' Server is listening on port 8080');
 // });
 
-// USING EXPRESS
-import express from "express"
-import { WebSocketServer } from "ws"
-
-
-const app = express();
-const httpserver = app.listen(8080)
-
-const wss = new WebSocketServer({ server: httpserver })
-
-wss.on("connection", (ws) => {
-
-    ws.on("error", console.error)
-
-    ws.on("message", (data, isbinary) => {
-        console.log("received :%s", data)
-    });
-})

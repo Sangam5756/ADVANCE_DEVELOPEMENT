fa8568d470f7f48c115b131ef5fd33d52eff8553
sangam5756 <sangammunde3@gmail.com>
2024-12-03 12:44:13 +0530

    session creation password hashing

--- Week 17/paytm/apps/user-app/app/page.tsx
+++ Week 17/paytm/apps/user-app/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import {Appbar} from "@repo/ui/appbar";
+import React from "react";
+import { signOut, signIn, useSession } from "next-auth/react";
+const page = () => {
+  const session = useSession();
+  console.log(session)
   return (
     <div>
-sdff
+      <Appbar
+        onSignin={signIn}
+        onSignout={signOut}
+        user={session?.data?.user}
+      />
     </div>
   );
-}
+};
+
+export default page;

d0b293ac9dfe6f7f10c88a325170c0d87bdbaf5d
sangam5756 <sangammunde3@gmail.com>
2024-12-09 08:13:01 +0530

    learn listening on redis when user submit data

--- Week 19/redis/express-backend/src/index.ts
+++ Week 19/redis/express-backend/src/index.ts
@@ -1,47 +1,35 @@
-import express from "express"
-import { createClient } from "redis"
-
+import express from "express";
+import { createClient } from "redis";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const client = createClient();
 
-
-app.get("/get", async (req, res) => {
-
-    const sub = await client.RPOP("submissions");
-    res.json({
-        sub: sub
-    })
-
-
-})
-
 app.post("/submit", async (req, res) => {
+    try {
+        const { problemId, userId, code, language } = req.body;
 
-    const { problemId, userId, code, language } = req.body;
-
-    await client.lPush("submissions", JSON.stringify({ problemId, userId, code, language }))
-    res.json({
-        message: "submission received!"
-    });
-
-
-
-
-})
-
+        await client.lPush(
+            "submissions",
+            JSON.stringify({ problemId, userId, code, language })
+        );
+        res.json({
+            message: "submission received!",
+        });
+    } catch (error) {
+        res.json({
+            message: "error in submission",
+        });
+    }
+});
 
 async function startserver() {
-
     await client.connect();
-    console.log("Connected to redis")
+    console.log("Connected to redis");
 
     app.listen(3000, () => {
-        console.log("server is started at port 3000")
-    })
-
+        console.log("server is started at port 3000");
+    });
 }
-
 
 startserver();

9c910c4c1b8dd532df1128726d97fd7836cdfcb1
sangam5756 <sangammunde3@gmail.com>
2024-12-23 21:48:45 +0530

    unit test done and expressApp test done

---
+++ Week 24/Testing-Mern/testing-ExpressApp/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+
+export const app = express();
+
+app.use(express.json());
+
+
+app.post('/sum', (req, res) => {
+    const { a, b } = req.body;
+
+    const result = a + b;
+    res.json({
+        result
+    });
+
+});
